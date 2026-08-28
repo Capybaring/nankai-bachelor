8d9be51a715cf898e931fcc3b7a56712ef6e35b0
Akana Rika <168892576+Capybaring@users.noreply.github.com>
2026-08-28 10:13:51 +0800

    Merge AIoT_UNet source snapshot

---
+++ AIoT_UNet/UNet_Pytorch/utils/data.py
@@ -0,0 +1,55 @@
+import numpy as np
+import skimage.exposure as exposure
+import skimage.io as io
+import skimage.color as color
+import torch
+from torch.utils.data import Dataset
+
+
+def contrast_and_reshape(img):
+    img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
+    return np.array(img_adapteq)
+
+
+class JB_Dataset(Dataset):
+    def __init__(
+        self, batch_size, img_size, input_img_paths, label_img_paths, contrast=True
+    ):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.label_img_paths = label_img_paths
+        self.contrast = contrast
+        assert len(input_img_paths) == len(label_img_paths)
+        print("Nb of images : {}".format(len(input_img_paths)))
+
+    def __len__(self):
+        return len(self.input_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """
+        Returns tuple (input, target) correspond to batch #idx.
+        """
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_label_img_paths = self.label_img_paths[i : i + self.batch_size]
+        x = np.zeros(
+            (self.batch_size, 1, self.img_size, self.img_size), dtype="float32"
+        )
+        for j, path in enumerate(batch_input_img_paths):
+            img = np.array(io.imread(path)) / 255
+            if len(img.shape) == 3:  # 如果是RGB图像
+                img = color.rgb2gray(img)  # 转换为灰度图像
+            if self.contrast:
+                img = contrast_and_reshape(img)
+            x[j] = np.expand_dims(img, 0)
+
+        y = np.zeros(
+            (self.batch_size, 1, self.img_size, self.img_size), dtype="float32"
+        )
+        for k, path_lab in enumerate(batch_label_img_paths):
+            img = np.array(io.imread(path_lab)) / 255
+            if len(img.shape) == 3:  # 如果是RGB图像
+                img = color.rgb2gray(img)  # 转换为灰度图像
+            y[k] = np.expand_dims(img, 0)
+        return torch.Tensor(x), torch.Tensor(y)
